fec5ec881e509fbd9b91e887897101151b091084
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-02 23:05:06 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Tahir Atakan Can on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
