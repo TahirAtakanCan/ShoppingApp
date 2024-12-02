30ecb3c0c163a8b15f9dc1d50adea9b55069f841
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-02 23:54:17 +0300

    MVVM design folder

---
+++ ShoppingApp/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ShoppingApp
+//
+//  Created by Tahir Atakan Can on 2.12.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
