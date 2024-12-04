76105a9266c3622d7ab8199ccb970691dab67f05
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-04 20:12:43 +0300

    feat: WelcomeView design and UIExtension added

--- ShoppingApp/ShoppingAppApp.swift
+++ ShoppingApp/ShoppingAppApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct ShoppingAppApp: App {
     var body: some Scene {
         WindowGroup {
-            WelcomeView()
+            
+            
+            
+            NavigationView{
+                WelcomeView()
+            }
         }
     }
 }
--- ShoppingApp/View/Login/WelcomeView.swift
+++ ShoppingApp/View/Login/WelcomeView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text("Hello World!")
+        ZStack{
+            Image("welcom_bg")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .screenWidth, height: .screenHeight)
+            
+            VStack{
+                Spacer()
+                
+                Image("app_logo")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .padding(.bottom, 8)
+                
+                Text("Welcome\nto out store")
+                    .font(.customfont(.semibold, fontSize: 48))
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("Ger your groceries in as fast as one hour")
+                    .font(.customfont(.medium, fontSize: 16))
+                    .foregroundStyle(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                
+                Spacer()
+                    .frame(height: 60)
+            }
+            .padding(.horizontal, 20)
+        }
+        .ignoresSafeArea()
     }
 }
 
