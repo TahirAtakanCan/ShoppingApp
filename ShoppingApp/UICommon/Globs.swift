898f2f226ec6e6ada7cbda6e63d94af4032fc4cb
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-09 11:31:28 +0300

    feat: Service Call Login func

---
+++ ShoppingApp/UICommon/Globs.swift
@@ -0,0 +1,24 @@
+//
+//  Globs.swift
+//  ShoppingApp
+//
+//  Created by Tahir Atakan Can on 9.12.2024.
+//
+
+import SwiftUI
+
+struct Globs {
+    static let AppName = ""
+    
+    static let BASE_URL = "http://localhost:3001/api/app/"
+    
+    static let SV_LOGIN = BASE_URL + "login"
+    
+}
+
+struct KKey {
+    static let status = "status"
+    static let message = "message"
+    static let payload = "payload"
+}
+
--- ShoppingApp/ViewModel/MainViewModel.swift
+++ ShoppingApp/ViewModel/MainViewModel.swift
@@ -14,4 +14,29 @@ class MainViewModel: ObservableObject {
     @Published var txtPassword: String = ""
     @Published var isShowPassword: Bool = false
     
+    @Published var showError = false
+    @Published var errorMessage = ""
+    
+    
+    //MARK: ServiceCall
+    
+    func serviceCallLogin() {
+        ServiceCall.post(parameter: ["email": txtEmail, "password": txtPassword], path: Globs.SV_LOGIN) { responseObj in
+            if let response = responseObj as? NSDictionary {
+                if response.value(forKey: KKey.status) as? String ?? "" == "1" {
+                    print(response)
+                    self.errorMessage = response.value(forKey: KKey.message) as? String ?? "Success"
+                    self.showError = true
+                    
+                }else{
+                    self.errorMessage = response.value(forKey: KKey.message) as? String ?? "Fail"
+                    self.showError = true
+                }
+            }
+        }failure: { error in
+            self.errorMessage = error?.localizedDescription ?? "Fail"
+            self.showError = true
+        }
+    }
+    
 }
