20e6bf5146b8f64328817e337d177e59eed3ff3a
Tahir Atakan Can <atkan4202@gmail.com>
2025-01-02 09:38:37 +0300

    homeview model

---
+++ ShoppingApp/ViewModel/HomeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModel.swift
+//  ShoppingApp
+//
+//  Created by Tahir Atakan Can on 2.01.2025.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewModel()
+}
